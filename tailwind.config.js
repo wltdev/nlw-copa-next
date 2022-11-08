967256c08c53beb73d7e5a55346fd82a285fb92f
Welinton Dias <mdias.welinton@gmail.com>
2022-11-08 12:47:58 -0300

    feat:base tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'Roboto, sans-serif'
+      },
+
+      gray: {
+        900: '#121214'
+      }
+    },
+  },
+  plugins: [],
+}
